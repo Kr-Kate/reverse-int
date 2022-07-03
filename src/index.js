6b92a603ae2db0b6b490d60d410126c16f6ef703
Kr-Kate <loly1995pop@gmail.com>
2022-07-03 19:56:23 +0400

    feat: done a task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = 0;
+    const absoluteNumber = Math.abs(n);
+    let varyNumber = String(absoluteNumber);
+    for(let i = 0; i < String(absoluteNumber).length; i++) {
+        let lastNumber = varyNumber[varyNumber.length - 1];
+        result = `${result}${lastNumber}`;
+        varyNumber = varyNumber.slice(0, -1);
+    }
+    return Number(result);
 }
